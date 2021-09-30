e23e4b876765f9cd302d1cda5301a1e5c681d3f6
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2021-10-01 02:12:58 +0500

    app updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -50,33 +50,72 @@ function About() {
       <div className="press-release-padding-1 mobile-padding">
         <div className="sm:pt-16 xl:pt-1 sm:mt-6">
           <p className="mb-4 xl:ml-2">
-          Welcome! Thank you for stopping by – I appreciate your interest in learning more about me and my Wireless Outdoor Cinema business.
-        </p>
-        <p className="xl:text-2xl font-bold mb-2 xl:ml-2">Growing up</p>
+            Welcome! Thank you for stopping by – I appreciate your interest in
+            learning more about me and my Wireless Outdoor Cinema business.
+          </p>
+          <p className="xl:text-2xl font-bold mb-2 xl:ml-2">Growing up</p>
           <p className="mb-4 xl:ml-2">
-          I’m originally from Vermont, and growing up; I split my time between Vermont and Maine. My childhood memories are filled with unforgettable times spent with my family and friends outdoors – exploring in the woods, observing wildlife, swimming, fishing, hiking, and camping outside. 
-        </p>
+            I’m originally from Vermont, and growing up; I split my time between
+            Vermont and Maine. My childhood memories are filled with
+            unforgettable times spent with my family and friends outdoors –
+            exploring in the woods, observing wildlife, swimming, fishing,
+            hiking, and camping outside.
+          </p>
           <p className="mb-4 xl:ml-2">
-          When I was young, I started learning about the construction trade watching my grandfather and father do miscellaneous projects and repairs around our house and at the summer camp in Bridgton, Maine. Watching my Dad and Grandfather build and fix things piqued my interest, and my Dad started to teach me the basics. By the time I was a teen, I was building waterfront dock systems, decks and repairing things for neighbors and friends.
-        </p>
-        <p className="xl:text-2xl font-bold mb-2 xl:ml-2">Learning from best</p>
-        <p className="mb-4 xl:ml-2">
-          In high school, I had a great Woodshop Teacher named David Tieff – a master woodcarver and woodworker. I was inspired by his work and took every available woodshop class he offered. Mr. Tieff taught me how to build furniture, wood carve, and use every type of woodworking power tool and machine available in the state-of-art woodshop. I remember always dreaming of having my own woodshop someday. After high school, I worked for various home builders and quickly learned the building trade. I even helped build a couple of log homes, one of which I built with my family and now live in it full-time in beautiful Bridgton, Maine.
-        </p>
-        <div className="xl:pt-4 desktop">
+            When I was young, I started learning about the construction trade
+            watching my grandfather and father do miscellaneous projects and
+            repairs around our house and at the summer camp in Bridgton, Maine.
+            Watching my Dad and Grandfather build and fix things piqued my
+            interest, and my Dad started to teach me the basics. By the time I
+            was a teen, I was building waterfront dock systems, decks and
+            repairing things for neighbors and friends.
+          </p>
+          <p className="xl:text-2xl font-bold mb-2 xl:ml-2">
+            Learning from best
+          </p>
+          <p className="mb-4 xl:ml-2">
+            In high school, I had a great Woodshop Teacher named David Tieff – a
+            master woodcarver and woodworker. I was inspired by his work and
+            took every available woodshop class he offered. Mr. Tieff taught me
+            how to build furniture, wood carve, and use every type of
+            woodworking power tool and machine available in the state-of-art
+            woodshop. I remember always dreaming of having my own woodshop
+            someday. After high school, I worked for various home builders and
+            quickly learned the building trade. I even helped build a couple of
+            log homes, one of which I built with my family and now live in it
+            full-time in beautiful Bridgton, Maine.
+          </p>
+          <div className="xl:pt-4 desktop">
             <img src="/second.png" alt="" />
             <p className="image-text-color image-text-press-1 text-sm pt-3 xl:ml-2">
-              Todd Morton preparing cuts on a table saw at his property in Bridgton, ME.
+              Todd Morton preparing cuts on a table saw at his property in
+              Bridgton, ME.
             </p>
           </div>
-          <p className="xl:text-2xl font-bold mb-2 mt-6 xl:ml-2">My professional career</p>
+          <p className="xl:text-2xl font-bold mb-2 mt-6 xl:ml-2">
+            My professional career
+          </p>
           <p className="mb-4 xl:ml-2">
-          At age 23, I took business and marketing courses at Champlain College in Burlington, Vermont, to help with my business knowledge. After college, I self-taught myself all about the emerging internet and built my first website, and started learning about search engine optimization and how to get websites found online. I then moved into the Internet Marketing field and had the opportunity to work for some well-known high technology companies such as Lycos – the internet search company (remember the Black Lab?) and Dragon Software (the voice-to-text software), a division of Nuance Communications – now owned by Microsoft. 
-        </p>
-        <p className="xl:text-2xl font-bold mb-2 xl:ml-2">The Outdoor Movie Theater idea</p>
+            At age 23, I took business and marketing courses at Champlain
+            College in Burlington, Vermont, to help with my business knowledge.
+            After college, I self-taught myself all about the emerging internet
+            and built my first website, and started learning about search engine
+            optimization and how to get websites found online. I then moved into
+            the Internet Marketing field and had the opportunity to work for
+            some well-known high technology companies such as Lycos – the
+            internet search company (remember the Black Lab?) and Dragon
+            Software (the voice-to-text software), a division of Nuance
+            Communications – now owned by Microsoft.
+          </p>
+          {/* <p className="xl:text-2xl font-bold mb-2 xl:ml-2">The Outdoor Movie Theater idea</p> */}
           <p className="xl:ml-2">
-          After 25 years of working in the Internet Marketing field, my desire to return to my roots frequently occupied my thoughts. I decided to take time off from the corporate world and strategized on my next career move. I knew I wanted to work for myself and be outside in nature versus being tethered to a desk in an unhealthy corporate environment.
-        </p>
+            After 25 years of working in the Internet Marketing field, my desire
+            to return to my roots frequently occupied my thoughts. I decided to
+            take time off from the corporate world and strategized on my next
+            career move. I knew I wanted to work for myself and be outside in
+            nature versus being tethered to a desk in an unhealthy corporate
+            environment.
+          </p>
         </div>
       </div>
       <div className="quote-box mt-8 sm:mt-16 quote-background mobile">
@@ -101,15 +140,33 @@ function About() {
       <div className="press-release-padding-1 mobile-padding">
         <div className="sm:pt-16 xl:pt-5 sm:pt-8 xl:ml-2">
           <h2 className="sm:mb-4 xl:mb-3 text-xl sm:text-2xl font-bold product-title heading-outdoor">
-          The Outdoor Movie Theater <span className="solution-orange">Idea</span>
+            The Outdoor Movie Theater{" "}
+            <span className="solution-orange">Idea</span>
           </h2>
           <p className="mb-4">
-          My entrepreneurial wheels started to turn. I wanted to set up an outdoor movie in my backyard for years and started researching online companies that provided outdoor cinemas. Unfortunately, the companies I found online only sold inflatable air screen cinemas and wanted a better solution that was easy to set up and start watching without a lengthy setup. I spent two years researching and testing different outdoor cinema equipment and fine-tuning my outdoor movie theater solution. Finally, I developed a simple to use, high-quality outdoor movie theater that could be used year-round – even in the cold winter months. 
-        </p>
+            My entrepreneurial wheels started to turn. I wanted to set up an
+            outdoor movie in my backyard for years and started researching
+            online companies that provided outdoor cinemas. Unfortunately, the
+            companies I found online only sold inflatable air screen cinemas and
+            wanted a better solution that was easy to set up and start watching
+            without a lengthy setup. I spent two years researching and testing
+            different outdoor cinema equipment and fine-tuning my outdoor movie
+            theater solution. Finally, I developed a simple to use, high-quality
+            outdoor movie theater that could be used year-round – even in the
+            cold winter months.
+          </p>
           <p className="mb-4">
-          I created a weather-resistant movie screen frame built from pressure-treated 4” x 4” wood posts and added post and beam hardware so it would never rust. The final product is strong, durable, can handle any weather condition Mother Nature throws at it and looks fantastic on my property. The movie screen frame never has to be broken down and stays up for year-round use, making it easy to attach and remove the cinematic screen when you want to watch movies. After a year of designing and building various frame systems, I developed one that I knew homeowners would love.
-        </p>
-      </div>
+            I created a weather-resistant movie screen frame built from
+            pressure-treated 4” x 4” wood posts and added post and beam hardware
+            so it would never rust. The final product is strong, durable, can
+            handle any weather condition Mother Nature throws at it and looks
+            fantastic on my property. The movie screen frame never has to be
+            broken down and stays up for year-round use, making it easy to
+            attach and remove the cinematic screen when you want to watch
+            movies. After a year of designing and building various frame
+            systems, I developed one that I knew homeowners would love.
+          </p>
+        </div>
       </div>
       <div className="no-padding container mx-auto">
         <div
@@ -128,11 +185,26 @@ function About() {
       <div className="press-release-padding-1 mobile-padding">
         <div className="pt-8 xl:ml-2">
           <p className="mb-4">
-          After developing a solid frame system, the next challenge was figuring out how to organize all of my cinema equipment, such as the projector, DVD player, streaming media player, digital tuner, transmitter, speakers, and more. I searched endlessly online for an outdoor projector and equipment stand solution and only found tripod projector stands with enough room to hold an outdoor projector and nothing else.
-        </p>
+            After developing a solid frame system, the next challenge was
+            figuring out how to organize all of my cinema equipment, such as the
+            projector, DVD player, streaming media player, digital tuner,
+            transmitter, speakers, and more. I searched endlessly online for an
+            outdoor projector and equipment stand solution and only found tripod
+            projector stands with enough room to hold an outdoor projector and
+            nothing else.
+          </p>
           <p className="mb-4">
-          Discouraged with the results I found online for projector stand solutions, I began designing various outdoor projector stands that could hold all of the cinema electronic components on. I built over ten different projector stand models and tested them over a year. The final result was a beautiful cedar wood projector and equipment stand that neatly organized all of my cinema equipment. In addition, the projector stand is lightweight, easy to carry, weather-resistant, and has an adjustable projector shelf to get the proper viewing angle to reach the cinematic screen – the only one of its kind on the market.
-        </p>
+            Discouraged with the results I found online for projector stand
+            solutions, I began designing various outdoor projector stands that
+            could hold all of the cinema electronic components on. I built over
+            ten different projector stand models and tested them over a year.
+            The final result was a beautiful cedar wood projector and equipment
+            stand that neatly organized all of my cinema equipment. In addition,
+            the projector stand is lightweight, easy to carry,
+            weather-resistant, and has an adjustable projector shelf to get the
+            proper viewing angle to reach the cinematic screen – the only one of
+            its kind on the market.
+          </p>
         </div>
       </div>
       <div className="no-padding container mx-auto">
@@ -150,17 +222,28 @@ function About() {
       </div>
       <div className="press-release-padding-1 mobile-padding thankyou">
         <div className="pt-8 xl:pt-8 sm:pt-16 xl:ml-2">
-        <p className="mb-4">
-        The cinema equipment stays on the projector stand and never needs to be removed - similar to an entertainment center inside your home. Store the projector stand with everything on it inside your house, and when you want to have a movie night, carry the stand outside, power it on, and start watching immediately. After the movie, you’ll need to take the projector stand back inside, as the cinema equipment is not waterproof.
-        </p>
-        <p className="mb-4">
-        I’ve been thrilled with the positive feedback on my wireless outdoor cinema business from customers, family, friends, and the media. I hope we get an opportunity to get to know each other and explore the possibilities of building your very own dream backyard movie theater.
-        </p>
-        <p className="mb-4">
-          Thanks for taking the time to read about me, and don’t hesitate to reach out with any questions you might have.
-        </p>
-        <p className="mb-4">My best,</p>
-        <img src={signatureBlack} className="h-10 mb-16" />
+          <p className="mb-4">
+            The cinema equipment stays on the projector stand and never needs to
+            be removed - similar to an entertainment center inside your home.
+            Store the projector stand with everything on it inside your house,
+            and when you want to have a movie night, carry the stand outside,
+            power it on, and start watching immediately. After the movie, you’ll
+            need to take the projector stand back inside, as the cinema
+            equipment is not waterproof.
+          </p>
+          <p className="mb-4">
+            I’ve been thrilled with the positive feedback on my wireless outdoor
+            cinema business from customers, family, friends, and the media. I
+            hope we get an opportunity to get to know each other and explore the
+            possibilities of building your very own dream backyard movie
+            theater.
+          </p>
+          <p className="mb-4">
+            Thanks for taking the time to read about me, and don’t hesitate to
+            reach out with any questions you might have.
+          </p>
+          <p className="mb-4">My best,</p>
+          <img src={signatureBlack} className="h-10 mb-16" />
         </div>
       </div>
     </Layout>
