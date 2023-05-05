a41beb28a441694ce2185b9d3481913eca84205e
Igor Abdulović <igorabdul@gmail.com>
2023-05-05 12:03:18 +0200

    image and subnav css update

--- src/pages/how-to-build-an-outdoor-movie-theater.js
+++ src/pages/how-to-build-an-outdoor-movie-theater.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Background from "../images/background-how-to.png"
+import Background from "../images/timberline-outdoor-movie-theater-1920px-x-1080px.jpg"
 
 function HowToBuildAnOutdoorMovieTheater() {
   function Accordion({ title, className, children }) {
@@ -17,19 +17,20 @@ function HowToBuildAnOutdoorMovieTheater() {
     return (
       <div className={`accordion text-white ${className}`}>
         <div
-          className="accordion-header container flex py-2"
+          className="accordion-header h-10 container flex items-center text-sm"
           onClick={toggleAccordion}
         >
           <div className="w-full flex-1 flex justify-between pr-3">
             <h3>{title}</h3>
             <button>{isOpen ? "-" : "+"}</button>
           </div>
-          <span
-            className="px-2 xl:px-4 ml-1 xl:ml-4 flex-0"
+          <a
+            href="#"
+            className="px-2 py-1 xl:px-4 ml-1 xl:ml-4 flex-0 button"
             style={{ backgroundColor: "#ff5400", borderRadius: "7px" }}
           >
             Get Now
-          </span>
+          </a>
         </div>
         <div
           className={`accordion-content container w-full absolute ${
@@ -57,12 +58,24 @@ function HowToBuildAnOutdoorMovieTheater() {
         className="mobile-subnav z-10 sticky top-0 block sm:hidden"
       >
         <ul className="flex flex-col justify-between w-full">
-          <li className="py-2 border-b text-sm">What You Get</li>
-          <li className="py-2 border-b text-sm">Frame Systems</li>
-          <li className="py-2 border-b text-sm">Cinema Uses</li>
-          <li className="py-2 border-b text-sm">Features</li>
-          <li className="py-2 border-b text-sm">Comparison</li>
-          <li className="pt-2 pb-4 text-sm">FAQ</li>
+          <li className="py-2 border-b text-sm">
+            <a href="#">What You Get</a>
+          </li>
+          <li className="py-2 border-b text-sm">
+            <a href="#">Frame Systems</a>
+          </li>
+          <li className="py-2 border-b text-sm">
+            <a href="#">Cinema Uses</a>
+          </li>
+          <li className="py-2 border-b text-sm">
+            <a href="#">Features</a>
+          </li>
+          <li className="py-2 border-b text-sm">
+            <a href="#">Comparison</a>
+          </li>
+          <li className="pt-2 pb-4 text-sm">
+            <a href="#">FAQ</a>
+          </li>
         </ul>
       </Accordion>
       <div className="desktop-subnav z-10 sticky top-0 hidden sm:block">
@@ -73,28 +86,43 @@ function HowToBuildAnOutdoorMovieTheater() {
             </div>
             <ul className="flex justify-between xl:justify-end text-xs xl:text-sm h-full items-center w-full xl:w-auto">
               <li className="mr-1 xl:mr-4">
-                <span className="hidden lg:block">What You Get</span>
-                <span className="block lg:hidden">What You Get</span>
+                <a href="#">
+                  <span className="hidden lg:block">What You Get</span>
+                  <span className="block lg:hidden">What You Get</span>
+                </a>
               </li>
               <li className="mx-1 xl:mx-4">
-                <span className="hidden lg:block">Frame Systems</span>
-                <span className="block lg:hidden">Frames</span>
+                <a href="#">
+                  <span className="hidden lg:block">Frame Systems</span>
+                  <span className="block lg:hidden">Frames</span>
+                </a>
               </li>
               <li className="mx-1 xl:mx-4">
-                <span className="hidden lg:block">Cinema Uses</span>
-                <span className="block lg:hidden">Uses</span>
+                <a href="#">
+                  <span className="hidden lg:block">Cinema Uses</span>
+                  <span className="block lg:hidden">Uses</span>
+                </a>
               </li>
-              <li className="mx-1 xl:mx-4">Features</li>
               <li className="mx-1 xl:mx-4">
-                <span className="hidden lg:block">Comparison</span>
-                <span className="block lg:hidden">Compare</span>
+                <a href="#">Features</a>
               </li>
-              <li className="mx-1 xl:mx-4">FAQ</li>
-              <li
-                className="px-2 xl:px-4 ml-1 xl:ml-4"
-                style={{ backgroundColor: "#ff5400", borderRadius: "7px" }}
-              >
-                Get Now
+              <li className="mx-1 xl:mx-4">
+                <a href="#">
+                  <span className="hidden lg:block">Comparison</span>
+                  <span className="block lg:hidden">Compare</span>
+                </a>
+              </li>
+              <li className="mx-1 xl:mx-4">
+                <a href="#">FAQ</a>
+              </li>
+              <li className="ml-1 xl:ml-4">
+                <a
+                  href="#"
+                  className="px-2 py-1 xl:px-4 button"
+                  style={{ backgroundColor: "#ff5400", borderRadius: "7px" }}
+                >
+                  Get Now
+                </a>
               </li>
             </ul>
           </div>
