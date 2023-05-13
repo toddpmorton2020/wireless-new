26a9d56035ca5e25098f20616c38aeea4140c9bd
Igor Abdulović <igorabdul@gmail.com>
2023-05-13 14:12:49 +0200

    style update

--- src/components/svg/HeresWhatYouGet/design-plan-icon.js
+++ src/components/svg/HeresWhatYouGet/design-plan-icon.js
@@ -16,7 +16,7 @@ export const DesignPlanIcon = ({ className }) => (
               d="m164.6,87.9l-.4,2-27.9,132.5c-1.1,5.4-6.4,8.9-11.9,7.7-5.4-1.2-8.9-6.5-7.7-11.9l28.9-137.2h0"
               style={{
                 fill: "none",
-                stroke: "#304e62",
+                stroke: "#ffffff",
                 strokeLinecap: "round",
                 strokeLinejoin: "round",
                 strokeWidth: "10.1px",
@@ -29,7 +29,7 @@ export const DesignPlanIcon = ({ className }) => (
               y2="246.4"
               style={{
                 fill: "none",
-                stroke: "#304e62",
+                stroke: "#ffffff",
                 strokeLinecap: "round",
                 strokeLinejoin: "round",
                 strokeWidth: "10.1px",
@@ -41,7 +41,7 @@ export const DesignPlanIcon = ({ className }) => (
               d="m163.8,87.8l.4,2,27.9,132.5c1.2,5.4,6.5,8.9,11.9,7.7,5.4-1.2,8.9-6.5,7.7-11.9l-28.7-136.3"
               style={{
                 fill: "none",
-                stroke: "#304e62",
+                stroke: "#ffffff",
                 strokeLinecap: "round",
                 strokeLinejoin: "round",
                 strokeWidth: "10.1px",
@@ -54,7 +54,7 @@ export const DesignPlanIcon = ({ className }) => (
               y2="246.4"
               style={{
                 fill: "none",
-                stroke: "#304e62",
+                stroke: "#ffffff",
                 strokeLinecap: "round",
                 strokeLinejoin: "round",
                 strokeWidth: "10.1px",
@@ -66,7 +66,7 @@ export const DesignPlanIcon = ({ className }) => (
           d="m194.9,57.8c0,16.6-13.5,30.1-30.1,30.1s-30.1-13.5-30.1-30.1,13.5-30.1,30.1-30.1,30.1,13.5,30.1,30.1Z"
           style={{
             fill: "none",
-            stroke: "#304e62",
+            stroke: "#ffffff",
             strokeLinecap: "round",
             strokeLinejoin: "round",
             strokeWidth: "10.1px",
@@ -76,7 +76,7 @@ export const DesignPlanIcon = ({ className }) => (
           d="m174.9,57.8c0,5.5-4.5,10-10,10s-10-4.5-10-10,4.5-10,10-10,10,4.5,10,10Z"
           style={{
             fill: "none",
-            stroke: "#304e62",
+            stroke: "#ffffff",
             strokeLinecap: "round",
             strokeLinejoin: "round",
             strokeWidth: "10.1px",
@@ -90,19 +90,19 @@ export const DesignPlanIcon = ({ className }) => (
         y2="5.1"
         style={{
           fill: "none",
-          stroke: "#304e62",
+          stroke: "#ffffff",
           strokeLinecap: "round",
           strokeLinejoin: "round",
           strokeWidth: "10.1px",
         }}
       />
     </g>
-    <g className="color-svg">
+    <g className="color-svg-blue">
       <path
         d="m5.1,272.5V99.9c0-16.1,13-29.1,29.1-29.1h28.4c3.9,0,7,3.1,7,7v158.6c0,3.9-3.1,7-7,7h-28.4c-16.1,0-29.1,13-29.1,29.1h0c0,16.1,13,29.1,29.1,29.1h260.1c16.1,0,29.1-13,29.1-29.1h0c0-16.1-13-29.1-29.1-29.1h-30.8c-3.9,0-7-3.1-7-7V77.8c0-3.9,3.1-7,7-7h30.8c16.1,0,29.1,13,29.1,29.1v172.6"
         style={{
           fill: "none",
-          stroke: "#304e62",
+          stroke: "#ffffff",
           strokeLinecap: "round",
           strokeLinejoin: "round",
           strokeWidth: "10.1px",
@@ -116,7 +116,7 @@ export const DesignPlanIcon = ({ className }) => (
           y2="89.2"
           style={{
             fill: "none",
-            stroke: "#304e62",
+            stroke: "#ffffff",
             strokeLinecap: "round",
             strokeLinejoin: "round",
             strokeWidth: "10.1px",
@@ -129,7 +129,7 @@ export const DesignPlanIcon = ({ className }) => (
           y2="89.2"
           style={{
             fill: "none",
-            stroke: "#304e62",
+            stroke: "#ffffff",
             strokeLinecap: "round",
             strokeLinejoin: "round",
             strokeWidth: "10.1px",
