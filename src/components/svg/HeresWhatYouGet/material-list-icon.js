1482ad8fbaab02d2b0344fc4fe70ab8989d83151
Igor Abdulović <igorabdul@gmail.com>
2023-05-13 13:59:58 +0200

    add icons

---
+++ src/components/svg/HeresWhatYouGet/material-list-icon.js
@@ -0,0 +1,192 @@
+import React from "react"
+
+export const MaterialListIcon = ({ className }) => (
+  <svg
+    id="Layer_1"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 96 88"
+    className={className}
+  >
+    <path
+      d="m12,72c0-.5-1.9-.8-3.2-.8-3.8,0-6.8,3.1-6.8,6.8V24c0-3.8,3.1-6,6.8-6s3.2.6,3.2,2v52Z"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <path
+      d="m84,72c0-.5,1.9-.8,3.2-.8,3.8,0,6.8,3.1,6.8,6.8V24c0-3.8-3.1-6-6.8-6s-3.2.6-3.2,2v52Z"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="40"
+      y1="26"
+      x2="12"
+      y2="26"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="84"
+      y1="26"
+      x2="78"
+      y2="26"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <path
+      d="m94,78c0,4.4-3.6,8-8,8H10c-4.4,0-8-3.6-8-8"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <rect
+      x="60"
+      y="2"
+      width="14"
+      height="72"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="52"
+      y1="26"
+      x2="60"
+      y2="26"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="68"
+      y1="12"
+      x2="74"
+      y2="12"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="66"
+      y1="22"
+      x2="74"
+      y2="22"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="68"
+      y1="32"
+      x2="74"
+      y2="32"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="68"
+      y1="42"
+      x2="74"
+      y2="42"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="66"
+      y1="52"
+      x2="74"
+      y2="52"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="68"
+      y1="62"
+      x2="74"
+      y2="62"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeLinejoin: "round",
+        strokeWidth: "4px",
+      }}
+    />
+    <path
+      d="m46,2c-3.3,0-6,1.8-6,2v58c0,2.6.7,5.2,2,7.4l4,6.6,4-6.6c1.3-2.2,2-4.8,2-7.4V4c0-.2-2.7-2-6-2Z"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeMiterlimit: "10",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="40"
+      y1="12"
+      x2="52"
+      y2="12"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeMiterlimit: "10",
+        strokeWidth: "4px",
+      }}
+    />
+    <line
+      x1="40"
+      y1="62"
+      x2="52"
+      y2="62"
+      style={{
+        fill: "none",
+        stroke: "#515151",
+        strokeMiterlimit: "10",
+        strokeWidth: "4px",
+      }}
+    />
+  </svg>
+)
